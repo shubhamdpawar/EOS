3e98d2c273db576d5da60e31bb661de4c3be90c0
shubhamdpawar <sdppawar78@gmail.com>
2021-11-29 02:47:31 +0530

    features add subtraction

--- arith/main.c
+++ arith/main.c
@@ -5,5 +5,8 @@ int main(int argc,char const *argv[])
     int result=0;
     result=add(10,20);
     printf("addition =%d",result);
+
+result=sub(10,20);
+    printf("subtraction =%d",result);
     return 0 ;
 }
---
+++ arith/sub.c
@@ -0,0 +1,6 @@
+int sub(int a,int b) 
+{
+    int diff = 0;
+    diff = a - b;
+    return diff;
+}
